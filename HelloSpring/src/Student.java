a80294faf782af96e83f370975a57ca2fba170f6
Ashok Reddy <akunam@italentdigital.com>
2017-09-01 17:46:30 +0530

    Spring Hello World App

---
+++ HelloSpring/src/Student.java
@@ -0,0 +1,17 @@
+
+public class Student {
+
+	String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	 public void displayInfo() {
+		// TODO Auto-generated method stub
+		 System.out.println("Hello "+name);
+	}
+}
